15eb9b96e4862167c99016288e090f0e1eef518f
John Else <john.else@gmail.com>
2017-07-01 19:27:11 +0100

    Update docs
    Signed-off-by: John Else <john.else@gmail.com>

--- lib/mvar.mli
+++ lib/mvar.mli
@@ -12,17 +12,17 @@ val take : 'a t -> 'a
 (** Non-blocking take - if the mvar holds no value, return None. *)
 val try_take : 'a t -> 'a option
 
-(** Wait until the mvar is empty, then put x in the mvar. *)
+(** Wait until the mvar is empty, then put the supplied value in the mvar. *)
 val put : 'a t -> 'a -> unit
 
-(** Non-blocking put - if the mvar is empty the put x in the mvar,
-    otherwise return false. *)
+(** Non-blocking put - if the mvar is empty the put the supplied value in the
+    mvar, otherwise return false. *)
 val try_put : 'a t -> 'a -> bool
 
 (** Test whether the mvar is empty. *)
 val is_empty : 'a t -> bool
 
-(** Wait until the mvar is populated, then set its value to x
+(** Wait until the mvar is populated, then put the supplied value in the mvar
     and return the previous value. *)
 val swap : 'a t -> 'a -> 'a
 
